8422c39f4af2d7024745270fa68165c7c7f305cd
unknown <github@canofsocks.com>
2025-07-07 20:15:21 +1000

    Ensure yt-dlp options are passed to new instance for multi-manifest stream

--- download_Live.py
+++ download_Live.py
@@ -1042,7 +1042,7 @@ class DownloadStream:
                             info_dict = None
                             live_status = None
                             try:
-                                info_dict, live_status = getUrls.get_Video_Info(self.id, wait=False, cookies=self.cookies)
+                                info_dict, live_status = getUrls.get_Video_Info(self.id, wait=False, cookies=self.cookies, additional_options=self.yt_dlp_options)
                                 
                             # If membership stream (without cookies) or privated, mark as end of stream as no more fragments can be grabbed
                             except PermissionError as e:
@@ -1200,7 +1200,7 @@ class DownloadStream:
             if temp_url_params.get("id", None) is not None and temp_url_params.get("id") != self.url_params.get("id"):
                 logging.warning("New manifest for format {0} detected, starting a new instance for the new manifest".format(self.format))
                 self.commit_batch(self.conn)
-                download_stream(info_dict=info_json, resolution=str(self.format).format(self.format), batch_size=self.batch_size, max_workers=self.max_workers, file_name="{0}.{1}".format(self.file_base_name, str(temp_url_params.get("id")).split('.')[-1]), keep_database=False, retries=self.fragment_retries, cookies=self.cookies)
+                download_stream(info_dict=info_json, resolution=str(self.format).format(self.format), batch_size=self.batch_size, max_workers=self.max_workers, file_name="{0}.{1}".format(self.file_base_name, str(temp_url_params.get("id")).split('.')[-1]), keep_database=False, retries=self.fragment_retries, cookies=self.cookies, yt_dlp_options=self.yt_dlp_options)
                 return True
             else:
                 return False
@@ -1211,7 +1211,7 @@ class DownloadStream:
             if temp_url_params.get("id", None) is not None and temp_url_params.get("id") != self.url_params.get("id"):
                 logging.warning("New manifest for resolution {0} detected, but not the same format as {1}, starting a new instance for the new manifest".format(self.resolution, self.format))
                 self.commit_batch(self.conn)
-                download_stream(info_dict=info_json, resolution=self.resolution, batch_size=self.batch_size, max_workers=self.max_workers, file_name="{0}.{1}".format(self.file_base_name, str(temp_url_params.get("id")).split('.')[-1]), keep_database=False, retries=self.fragment_retries, cookies=self.cookies)
+                download_stream(info_dict=info_json, resolution=self.resolution, batch_size=self.batch_size, max_workers=self.max_workers, file_name="{0}.{1}".format(self.file_base_name, str(temp_url_params.get("id")).split('.')[-1]), keep_database=False, retries=self.fragment_retries, cookies=self.cookies, yt_dlp_options=self.yt_dlp_options)
                 return True
         elif self.resolution != "audio_only" and YoutubeURL.Formats().getFormatURL(info_json=info_json, resolution="best", return_format=False) is not None:
             temp_stream_url = YoutubeURL.Formats().getFormatURL(info_json=info_json, resolution="best", return_format=False)
@@ -1220,7 +1220,7 @@ class DownloadStream:
             if temp_url_params.get("id", None) is not None and temp_url_params.get("id") != self.url_params.get("id"):
                 logging.warning("New manifest has been found, but it is not the same format or resolution".format(self.resolution, self.format))
                 self.commit_batch(self.conn)
-                download_stream(info_dict=info_json, resolution="best", batch_size=self.batch_size, max_workers=self.max_workers, file_name="{0}.{1}".format(self.file_base_name, str(temp_url_params.get("id")).split('.')[-1]), keep_database=False, retries=self.fragment_retries, cookies=self.cookies)
+                download_stream(info_dict=info_json, resolution="best", batch_size=self.batch_size, max_workers=self.max_workers, file_name="{0}.{1}".format(self.file_base_name, str(temp_url_params.get("id")).split('.')[-1]), keep_database=False, retries=self.fragment_retries, cookies=self.cookies, yt_dlp_options=self.yt_dlp_options)
                 return True
         return False
 
@@ -1701,7 +1701,7 @@ class DownloadStreamDirect:
                             info_dict = None
                             live_status = None
                             try:
-                                info_dict, live_status = getUrls.get_Video_Info(self.id, wait=False, cookies=self.cookies)
+                                info_dict, live_status = getUrls.get_Video_Info(self.id, wait=False, cookies=self.cookies, additional_options=self.yt_dlp_options)
                                 
                             # If membership stream (without cookies) or privated, mark as end of stream as no more fragments can be grabbed
                             except PermissionError as e:
@@ -1861,7 +1861,7 @@ class DownloadStreamDirect:
             if temp_url_params.get("id", None) is not None and temp_url_params.get("id") != self.url_params.get("id"):
                 logging.warning("New manifest for format {0} detected, starting a new instance for the new manifest".format(self.format))
                 #self.commit_batch(self.conn)
-                download_stream_direct(info_dict=info_json, resolution=str(self.format), batch_size=self.batch_size, max_workers=self.max_workers, file_name="{0}.{1}".format(self.file_base_name, str(temp_url_params.get("id")).split('.')[-1]), retries=self.fragment_retries, cookies=self.cookies)
+                download_stream_direct(info_dict=info_json, resolution=str(self.format), batch_size=self.batch_size, max_workers=self.max_workers, file_name="{0}.{1}".format(self.file_base_name, str(temp_url_params.get("id")).split('.')[-1]), retries=self.fragment_retries, cookies=self.cookies, yt_dlp_options=self.yt_dlp_options)
                 return True
             else:
                 return False
@@ -1872,7 +1872,7 @@ class DownloadStreamDirect:
             if temp_url_params.get("id", None) is not None and temp_url_params.get("id") != self.url_params.get("id"):
                 logging.warning("New manifest for resolution {0} detected, but not the same format as {1}, starting a new instance for the new manifest".format(self.resolution, self.format))
                 #self.commit_batch(self.conn)
-                download_stream_direct(info_dict=info_json, resolution=self.resolution, batch_size=self.batch_size, max_workers=self.max_workers, file_name="{0}.{1}".format(self.file_base_name, str(temp_url_params.get("id")).split('.')[-1]), retries=self.fragment_retries, cookies=self.cookies)
+                download_stream_direct(info_dict=info_json, resolution=self.resolution, batch_size=self.batch_size, max_workers=self.max_workers, file_name="{0}.{1}".format(self.file_base_name, str(temp_url_params.get("id")).split('.')[-1]), retries=self.fragment_retries, cookies=self.cookies, yt_dlp_options=self.yt_dlp_options)
                 return True
         elif self.resolution != "audio_only" and YoutubeURL.Formats().getFormatURL(info_json=info_json, resolution="best", return_format=False) is not None:
             temp_stream_url = YoutubeURL.Formats().getFormatURL(info_json=info_json, resolution="best", return_format=False)
@@ -1881,7 +1881,7 @@ class DownloadStreamDirect:
             if temp_url_params.get("id", None) is not None and temp_url_params.get("id") != self.url_params.get("id"):
                 logging.warning("New manifest has been found, but it is not the same format or resolution".format(self.resolution, self.format))
                 #self.commit_batch(self.conn)
-                download_stream_direct(info_dict=info_json, resolution="best", batch_size=self.batch_size, max_workers=self.max_workers, file_name="{0}.{1}".format(self.file_base_name, str(temp_url_params.get("id")).split('.')[-1]), retries=self.fragment_retries, cookies=self.cookies)
+                download_stream_direct(info_dict=info_json, resolution="best", batch_size=self.batch_size, max_workers=self.max_workers, file_name="{0}.{1}".format(self.file_base_name, str(temp_url_params.get("id")).split('.')[-1]), retries=self.fragment_retries, cookies=self.cookies, yt_dlp_options=self.yt_dlp_options)
                 return True
         return False
     # Function to combine segments into a single file
